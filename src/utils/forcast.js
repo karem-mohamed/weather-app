628758ee44d344840c31c6e7b1cc6a98ae0339bb
karem-mohamed <karem96mohamed@gmail.com>
2020-03-12 06:04:33 +0200

    add icon and degree

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -8,7 +8,7 @@ const url=`https://api.darksky.net/forecast/b3dcf2e5da682cab70160acca2c7b9e9/${l
         }else if(res.body.error){
             callback(err+"unable to find loaction",undefined)
         }else{
-            callback(undefined,`the temperature is ${res.body.currently.temperature} and humidity is ${res.body.currently.humidity}`)
+            callback(undefined,`the temperature is ${Math.round((res.body.currently.temperature-32)*5/9)}&#8451; and humidity is ${res.body.currently.humidity}`)
         }
     })
 }
